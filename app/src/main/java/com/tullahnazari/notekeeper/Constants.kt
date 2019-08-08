f6d68893ecde09761acee261e822a6e75c21cdb3
Mr. Tee <tullah.nazari@gmail.com>
2019-08-08 08:09:47 -0400

    fixed position for clicking items in a list

--- app/src/main/java/com/tullahnazari/notekeeper/Constants.kt
+++ app/src/main/java/com/tullahnazari/notekeeper/Constants.kt
@@ -1,4 +1,5 @@
 package com.tullahnazari.notekeeper
 
 const val EXTRA_NOTE_POSITION = "EXTRA_NOTE_POSITION"
-const val POSITION_NOT_SET = 1
+//professor had this set to 1 which caused an error when selecting the 1 place
+const val POSITION_NOT_SET = -1
