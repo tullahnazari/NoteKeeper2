557bc4efe8455eed1b6507b80b5341b8e294bf01
Mr. Tee <tullah.nazari@gmail.com>
2019-08-08 07:23:14 -0400

    App bar Actions
    Handling menu bar
    App bar actions

--- app/src/main/java/com/tullahnazari/notekeeper/DataManager.kt
+++ app/src/main/java/com/tullahnazari/notekeeper/DataManager.kt
@@ -3,30 +3,63 @@ package com.tullahnazari.notekeeper
 import com.tullahnazari.notekeeper.Model.CourseInfo
 import com.tullahnazari.notekeeper.Model.NoteInfo
 
-class DataManager {
+object DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
 
     init {
         initializeCourses()
+        initializeNotes()
+
     }
 
     private fun initializeCourses() {
 
-        var course = CourseInfo("android_intents",
-            "Android Programming with Intents")
-            courses.set(course.courseId, course)
-
-
-        course =  CourseInfo("android_async",
-            "Android Async Programming and Services")
+        var course = CourseInfo(courseId = "android_intents", title = "Android Programming with Intents")
         courses.set(course.courseId, course)
 
-        course = CourseInfo("Java Fundamentals: The platform", "java_lang")
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
         courses.set(course.courseId, course)
 
-        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        course = CourseInfo(courseId = "java_lang", title = "Java Fundamentals: The Java Language")
         courses.set(course.courseId, course)
 
+        course = CourseInfo(courseId = "java_core", title = "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+
+    }
+
+    private fun initializeNotes() {
+        var course = courses["android_intents"]!!
+        var note = NoteInfo(course, "Dynamic intent resolution",
+            "Wow, intents allow components to be resolved at runtime")
+        notes.add(note)
+        note = NoteInfo(course, "Delegating intents",
+            "PendingIntents are powerful; they delegate much more than just a component invocation")
+        notes.add(note)
+
+        course = courses["android_async"]!!
+        note = NoteInfo(course, "Service default threads",
+            "Did you know that by default an Android Service will tie up the UI thread?")
+        notes.add(note)
+        note = NoteInfo(course, "Long running operations",
+            "Foreground Services can be tied to a notification icon")
+        notes.add(note)
+
+        course = courses["java_lang"]!!
+        note = NoteInfo(course, "Parameters",
+            "Leverage variable-length parameter lists")
+        notes.add(note)
+        note = NoteInfo(course, "Anonymous classes",
+            "Anonymous classes simplify implementing one-use types")
+        notes.add(note)
+
+        course = courses["java_core"]!!
+        note = NoteInfo(course, "Compiler options",
+            "The -jar option isn't compatible with with the -cp option")
+        notes.add(note)
+        note = NoteInfo(course, "Serialization",
+            "Remember to include SerialVersionUID to assure version compatibility")
+        notes.add(note)
     }
 }
