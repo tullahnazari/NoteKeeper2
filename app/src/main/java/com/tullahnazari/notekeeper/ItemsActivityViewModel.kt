bb4b51a4d374b181539cdf89adbf4b1a7409684a
Mr. Tee <tullah.nazari@gmail.com>
2019-08-12 19:53:46 -0400

    Persisting and restoring instances on system destroy
    handling app when system destroys app instance with onSaveInstanceState

--- app/src/main/java/com/tullahnazari/notekeeper/ItemsActivityViewModel.kt
+++ app/src/main/java/com/tullahnazari/notekeeper/ItemsActivityViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 
 class ItemsActivityViewModel : ViewModel() {
 
+    var navDrawerDisplaySelectionName = "com.tullahnazari.notekeeper.ItemsActivityViewModel.navDrawerDisplaySelection"
+
     var navDrawerDisplaySelection = R.id.nav_notes
 }
